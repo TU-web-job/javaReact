0e31a312f180d7b2395d9b4ad135b7a831ae0734
tu <t.u.9293991@gmail.com>
2025-08-28 17:10:38 +0900

    画面追加

--- front/src/app/Diary.tsx
+++ front/src/app/Diary.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../components/Header';
+import { Section } from '../components/Section';
 
 const Diary = () => {
     return(
         <>
         <Header />
+        <Section />
         </>
     );
 }
